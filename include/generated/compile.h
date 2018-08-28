e619f92d8fd98da4382044ea3ff9c9fdb48d0941
Svyatoslav Ryhel <clamor95@gmail.com>
2018-08-28 12:11:14 +0300

    Initial bringup
    Signed-off-by: Svyatoslav Ryhel <clamor95@gmail.com>

---
+++ include/generated/compile.h
@@ -0,0 +1,7 @@
+/* This file is auto generated, version 0 */
+/* SMP PREEMPT */
+#define UTS_MACHINE "arm"
+#define UTS_VERSION "#0 SMP PREEMPT Tue Aug 28 12:05:21 EEST 2018"
+#define LINUX_COMPILE_BY "clamor"
+#define LINUX_COMPILE_HOST "retranslator"
+#define LINUX_COMPILER "gcc version 4.8 (GCC) "
